ac788adce95ccc472741ef2e43fa4b115a650ecc
Rex <101835887+RexGm@users.noreply.github.com>
2025-10-02 02:05:27 +0300

    Time: 282 ms (5.14%) | Memory: 45.4 MB (9.40%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -14,9 +14,10 @@ class Solution {
        int count = 0;
        String result = "";
         for(int i = index;  i < len; i++){
-            if(!result.contains( String.valueOf(s.charAt(i)))){
+            char ch = s.charAt(i);
+            if(!result.contains( String.valueOf(ch))){
                 count+=1;
-                result+= s.charAt(i);
+                result+= ch;
             }else{return count;}
 
         
